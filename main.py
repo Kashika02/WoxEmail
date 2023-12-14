487e243cccf37ed7d54f4bf0b8997b99cac86e5c
Kashika02 <69431618+Kashika02@users.noreply.github.com>
2023-12-14 18:49:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+import pandas as pd
 
 # Streamlit app
 st.title("Email Sender App")
 
 # Email configuration
-sender_email = "kashika.parmar@woxsen.edu.in"  # replace with your email
-sender_password = "Fah32413"  # replace with your password
+sender_email = "Woxsenuniversity@outlook.com"  # replace with your email
+sender_password = "Woxsen@123"  # replace with your password
 
 # Input box for email content
 email_content = st.text_area("Enter your email content here:", "")
@@ -17,22 +18,28 @@ email_content = st.text_area("Enter your email content here:", "")
 # Input box for subject
 email_subject = st.text_input("Enter the subject of the email:", "")
 
-# Input box for recipient email addresses (comma-separated)
-recipient_emails = st.text_input("Enter recipient email addresses (comma-separated):", "")
+# Input box for CC email addresses (comma-separated)
+cc_emails = st.text_input("Enter CC email addresses (comma-separated):", "")
 
+xlsx_file = st.sidebar.file_uploader("Upload XLSX File", type=["xlsx"])
 # Button to send emails
 if st.button("Send Emails"):
-
-    # Splitting email addresses
-    recipients = recipient_emails.split(",")
-
-    # SMTP setup
     try:
-        # Use Outlook SMTP server and port
+        # Read XLSX file
+        df = pd.read_excel(xlsx_file, engine='openpyxl')
+
+        # Extract email addresses from the "Father EmailId" column
+        recipients = df["Father EmailId"].astype(str).tolist()
+
+        # Splitting CC email addresses
+        cc_recipients = [cc.strip() for cc in cc_emails.split(",")]
+
+        # SMTP setup
         server = smtplib.SMTP("smtp-mail.outlook.com", 587)
         server.starttls()
         server.login(sender_email, sender_password)
 
+        # Send emails to main recipients
         for recipient in recipients:
             # Email content
             msg = MIMEMultipart()
@@ -42,8 +49,29 @@ if st.button("Send Emails"):
             body = email_content
             msg.attach(MIMEText(body, "plain"))
 
+            # Print email content and recipient email ID
+            print(f"Email Content: {body}")
+            print(f"Recipient Email ID: {recipient.strip()}")
+
             # Send email
             server.sendmail(sender_email, recipient.strip(), msg.as_string())
+
+        # Send emails to CC recipients
+        for cc_recipient in cc_recipients:
+            # Email content
+            cc_msg = MIMEMultipart()
+            cc_msg["From"] = sender_email
+            cc_msg["To"] = cc_recipient.strip()
+            cc_msg["Subject"] = email_subject  # Use the user-provided subject
+            cc_body = email_content
+            cc_msg.attach(MIMEText(cc_body, "plain"))
+
+            # Print email content and CC recipient email ID
+            print(f"Email Content: {cc_body}")
+            print(f"CC Recipient Email ID: {cc_recipient.strip()}")
+
+            # Send email
+            server.sendmail(sender_email, cc_recipient.strip(), cc_msg.as_string())
 
         st.success("Emails sent successfully!")
 
