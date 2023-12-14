567967ad8b27a82b00dc25d580ffc2ac477879ae
Kashika02 <69431618+Kashika02@users.noreply.github.com>
2023-12-14 09:29:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+# Streamlit app
+st.title("Email Sender App")
+
+# Email configuration
+sender_email ="kashika.parmar@woxsen.edu.in"  # replace with your email
+sender_password = "Fah32413"  # replace with your password
+
+# Input box for email content
+email_content = st.text_area("Enter your email content here:", "")
+
+# Input box for recipient email addresses (comma-separated)
+recipient_emails = st.text_input("Enter recipient email addresses (comma-separated):", "")
+
+# Button to send emails
+if st.button("Send Emails"):
+
+    # Splitting email addresses
+    recipients = recipient_emails.split(",")
+
+    # SMTP setup
+    try:
+        # Use Outlook SMTP server and port
+        server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+
+        for recipient in recipients:
+            # Email content
+            msg = MIMEMultipart()
+            msg["From"] = sender_email
+            msg["To"] = recipient.strip()
+            msg["Subject"] = "Subject of the email"
+            body = email_content
+            msg.attach(MIMEText(body, "plain"))
+
+            # Send email
+            server.sendmail(sender_email, recipient.strip(), msg.as_string())
+
+        st.success("Emails sent successfully!")
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+    finally:
+        server.quit()
